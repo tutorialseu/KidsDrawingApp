0c0969b2a558752dc63215131704a3135612a083
kulloveth <kulloveth@gmail.com>
2021-09-13 03:52:06 +0100

    add progress dialog

--- app/src/main/java/eu/tutorials/backgroundexecution/MainActivity.kt
+++ app/src/main/java/eu/tutorials/backgroundexecution/MainActivity.kt
@@ -13,13 +13,15 @@ import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
-
+    //Create a dialog variable
+    var customProgressDialog: Dialog? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnExecute:Button = findViewById(R.id.btn_execute)
         btnExecute.setOnClickListener {
+            showProgressDialog()
             lifecycleScope.launch {
                 execute("Task executed successfully.")
             }
@@ -29,11 +31,14 @@ class MainActivity : AppCompatActivity() {
 
 
     private suspend fun execute(result:String){
+        //TODO(You can code here what you wants to execute in background execution without freezing the UI
         withContext(Dispatchers.IO) {
+            // This is just a for loop which is executed for 1000000 times.
             for (i in 1..1000000) {
                 Log.e("delay : ", "" + i)
             }
             runOnUiThread {
+                cancelProgressDialog()
                 Toast.makeText(
                     this@MainActivity, result,
                     Toast.LENGTH_SHORT
@@ -42,6 +47,30 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Method is used to show the Custom Progress Dialog.
+     */
+    private fun showProgressDialog() {
+        customProgressDialog = Dialog(this@MainActivity)
+
+        /*Set the screen content from a layout resource.
+        The resource will be inflated, adding all top-level views to the screen.*/
+        customProgressDialog?.setContentView(R.layout.dialog_custom_progress)
+
+        //Start the dialog and display it on screen.
+        customProgressDialog?.show()
+    }
+
+
+    /**
+     * This function is used to dismiss the progress dialog if it is visible to user.
+     */
+    private fun cancelProgressDialog() {
+        if (customProgressDialog != null) {
+            customProgressDialog?.dismiss()
+            customProgressDialog = null
+        }
+    }
 
 
 
